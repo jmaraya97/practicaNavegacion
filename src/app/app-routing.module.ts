cd65a062541ade811457d6f59fc00ea3d5c08e14
jmaraya97 <jmaraya97@gmail.com>
2020-10-09 19:48:48 +0200

    version Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,22 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'pagina2',
+    loadChildren: () => import('./pagina2/pagina2.module').then( m => m.Pagina2PageModule)
+  },
+  {
+    path: 'pagina3',
+    loadChildren: () => import('./pagina3/pagina3.module').then( m => m.Pagina3PageModule)
+  },
+  {
+    path: 'pagina4',
+    loadChildren: () => import('./pagina4/pagina4.module').then( m => m.Pagina4PageModule)
+  },
+  {
+    path: 'pagina5',
+    loadChildren: () => import('./pagina5/pagina5.module').then( m => m.Pagina5PageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+ selector: 'app-home',
+ templateUrl: 'home.page.html',
+ styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+ constructor(private navCtrl: NavController){
 
+ }
+ goToPagina1(){
+  this.navCtrl.navigateForward('/pagina1');
+}
+ goToPagina2(){
+  this.navCtrl.navigateForward('/pagina2');
+ }
+ goToPagina3(){
+  this.navCtrl.navigateForward('/pagina3');
+}
+goToPagina4(){
+  this.navCtrl.navigateForward('/pagina4');
+}
+goToPagina5(){
+  this.navCtrl.navigateForward('/pagina5');
+}
 }
